c76597ee8515c5dc7cba20ef1bf73ed7f7527233
ElieVitrai <elie.vitrai@gmail.com>
2023-01-06 17:00:07 +0100

    temperature

---
+++ capteurs_volet/capteurs_volet.ino
@@ -0,0 +1,18 @@
+#include "DHT.h"
+DHT dht(18, DHT11);
+
+void setup() {
+   dht.begin();
+   Serial.begin(9600);
+}
+
+void loop() {
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+
+   Serial.print("Humidity: ");
+   Serial.println(h);
+   delay(2000);
+   Serial.print("Temperature: ");
+   Serial.println(t);
+}
